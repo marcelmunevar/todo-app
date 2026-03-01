8e9710231faaf50d9ce003fd94f834e64d14228b
Marcel Munevar <marcelmunevar@gmail.com>
2026-03-01 14:25:44 -0500

    added login fields

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,16 @@
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Container from "@mui/material/Container";
+
 function Login() {
-  return <h1>Login</h1>;
+  return (
+    <Container maxWidth="sm">
+      <Stack spacing={2}>
+        <h1>Sign in</h1>
+        <TextField id="outlined-basic" label="Username" variant="outlined" />
+        <TextField id="outlined-basic" label="Password" type="password" />
+      </Stack>
+    </Container>
+  );
 }
 export default Login;
